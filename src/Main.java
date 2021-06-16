f2767a7e44eed998251e32ead1e327d15b847057
Leonid Beltsov <leonidbeltsov@gmail.com>
2021-06-16 23:57:32 +0300

    fix 1

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int current_account = 100;
-        int payment = 1001; // <- тут вносится значение платежа внесенного клиентом
-        int payment_bonus;
-        if (payment>=1000) {
-            payment_bonus = payment/100;
-            current_account = current_account + payment + payment_bonus;
-            System.out.println("Cумма на счету клиента: " + current_account + " рублей");
-            System.out.println("Бонус составил: " + payment_bonus + " рублей");
-            }
-        else {
-            current_account = current_account + payment;
-            System.out.println("Cумма на счету клиента: " + current_account + " рублей");
+        int currentAccount = 100;
+        int payment = 1111; // <- тут вносится значение платежа внесенного клиентом
+        int paymentBonus;
+        if (payment >= 1000) {
+            paymentBonus = payment / 100;
+            currentAccount = currentAccount + payment + paymentBonus;
+            System.out.println("Cумма на счету клиента: " + currentAccount + " рублей");
+            System.out.println("Бонус составил: " + paymentBonus + " рублей");
+        } else {
+            currentAccount = currentAccount + payment;
+            System.out.println("Cумма на счету клиента: " + currentAccount + " рублей");
             System.out.println("Бонус составил: 0 рублей");
-            }
+        }
     }
 }
